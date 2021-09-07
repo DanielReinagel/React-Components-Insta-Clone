cf1a9294e713b4e56a44d2131e351767ee000e72
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-07 12:02:45 -0700

    MVP done

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -1,7 +1,7 @@
 // Look at the number of likes on line 26. Right now it's hard coded to '100'.
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
-// (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
+// (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.) NOT DONE
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
@@ -23,7 +23,7 @@ const LikeSection = props => {
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number' onClick={likePost}>{numberOfLikes} likes</p>
     </div>
   );
 };
